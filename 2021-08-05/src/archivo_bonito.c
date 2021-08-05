4b8c67745c22097904d6c84426c3c10b16bc4cf2
muffin <squishysparrow7.7@gmail.com>
2021-08-05 21:05:16 +1200

    First mystery exe, pretty good

---
+++ 2021-08-05/src/archivo_bonito.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <windows.h>
+int
+main()
+{
+    system("color 4f");
+    system("tree");
+    Sleep(500);
+    system("@echo off");
+    system("echo jaja hackeado lol");
+printf("Desencriptando llave privada RSA-PGP de internet por razones malevolas\r\n");
+    for (int i = 0; i < 26; i++)
+    {
+        printf("\r[");
+
+        for (int j = 0; j < i; j++)
+            putchar('#');
+
+        for (int j = 0; j < 25 - i; j++)
+            putchar('-');
+
+        printf("] %d%%", i * 4);
+        Sleep(150);
+    }
+    printf("\r\n");
+    system("echo COMPLETADO!");
+    system("echo borrando evidencia....");
+    system("start iexplore https://media1.tenor.com/images/23aeaaa34afd591deee6c163c96cb0ee/tenor.gif");
+    system("cls");
+    system("echo lol");
+
+    system("pause");
+    return 0;
+}
---
+++ 2021-08-05/src/not_suspicious.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <windows.h>
+int
+main()
+{
+    system("color 4f");
+    system("tree");
+    Sleep(500);
+    system("@echo off");
+    system("echo I'm a master hacker xD :sunglasses:");
+printf("Decrypting PGP RSA unprivate internet FTP key for nefarious purposes\r\n");
+    for (int i = 0; i < 26; i++)
+    {
+        printf("\r[");
+
+        for (int j = 0; j < i; j++)
+            putchar('#');
+
+        for (int j = 0; j < 25 - i; j++)
+            putchar('-');
+
+        printf("] %d%%", i * 4);
+        Sleep(150);
+    }
+    printf("\r\n");
+    system("echo COMPLETE!");
+    system("echo erasing evidence....");
+    system("start iexplore https://media1.tenor.com/images/23aeaaa34afd591deee6c163c96cb0ee/tenor.gif");
+    system("cls");
+    system("echo I'm just too funny");
+
+    system("pause");
+    return 0;
+}
